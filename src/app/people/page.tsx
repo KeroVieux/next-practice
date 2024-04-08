14f4debd58c22a5581b79593431d92b3b23981d7
keroVieux <bigboss@hidoge.cn>
2024-04-08 20:11:23 +0800

    added scss

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -16,7 +16,7 @@ export default function PeoplePage() {
                         </svg>
                     </button>
                 </div>
-                <div className="flex flex-col items-center pb-10">
+                <div className="flex flex-col items-center pb-10 test-box">
                     <img className="w-24 h-24 mb-3 rounded-full shadow-lg"
                          src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
                     <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
